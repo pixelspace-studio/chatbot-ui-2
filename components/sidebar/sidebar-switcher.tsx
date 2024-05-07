bec130f824b04198aabf67e1ebfbd733ad25ca10
alejandro <alex@pixelspace.com>
2024-05-07 09:21:18 -0600

    fix5

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -29,7 +29,7 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
   contentType
 }) => {
   return (
-    <div className="border-pixelspace-gray-60 bg-pixelspace-gray-90 flex h-[calc(100dvh)] flex-col justify-between border-r px-4 pb-6 pt-8">
+    <div className="border-pixelspace-gray-60 bg-pixelspace-gray-90 flex h-[calc(100dvh)] flex-col justify-between border-r px-5 pb-6 pt-8">
       <div className="mb-4 flex flex-col items-center justify-center">
         <Image
           unoptimized
