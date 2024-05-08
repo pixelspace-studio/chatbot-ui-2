19d5239a97a7d14f98f0b8a1b600138215444e1a
alejandro <alex@pixelspace.com>
2024-05-08 15:25:28 -0600

    fix8

--- components/chat/chat-ui.tsx
+++ components/chat/chat-ui.tsx
@@ -221,7 +221,7 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
       </div>
 
       <div
-        className="flex h-[calc(100dvh)] w-full flex-col overflow-auto overflow-x-hidden pl-6"
+        className={`flex h-[calc(100dvh)] w-full flex-col overflow-auto overflow-x-hidden ${isCollapsed ? "pl-6" : ""}`}
         onScroll={handleScroll}
       >
         <div ref={messagesStartRef} />
